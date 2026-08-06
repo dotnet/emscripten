3eadc71e87f01fafd641aa94fa90e41740e727ae
Alexander Köplinger <alex.koeplinger@outlook.com>
2026-08-06 12:02:12 +0200

    Remove sqlite from the MINIMAL embuilder list
    Backport of #69 to release/8.0

--- embuilder.py
+++ embuilder.py
@@ -66,8 +66,9 @@ MINIMAL_TASKS = [
     'crt1_proxy_main',
     'libunwind-except',
     'libnoexit',
-    'sqlite3',
-    'sqlite3-mt',
+# dotnet change, remove it, so that we don't download the sqlite3 source code in emsdk
+#    'sqlite3',
+#    'sqlite3-mt',
 ]
 
 # Additional tasks on top of MINIMAL_TASKS that are necessary for PIC testing on
